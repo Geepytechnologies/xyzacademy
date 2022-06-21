f364b8f8dd4248a4cf38f933f6cf0a8a9353e45a
Geepy <geepytechnologies@gmail.com>
2022-06-21 08:05:04 +0100

    changed

--- components/Comments.js
+++ components/Comments.js
@@ -1,29 +1,63 @@
 import React from 'react'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 import {AiOutlineComment} from 'react-icons/ai'
 
 export default function Comments() {
+    const settings = {
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        speed: 4000,
+        autoplaySpeed: 4000,
+        // cssEase: "linear",
+        slidesToShow: 1,
+        slidesToScroll: 1
+      };
   return (
     <div className='mb-[30px]'>
         <div className='flex flex-row justify-center items-center'>
             <AiOutlineComment style={{marginRight: 10,fill: 'hsl(34,100%,47%)',fontSize: 20}} />
             <h2 className='font-[600]'>What our students say</h2>
         </div>
-        <div className='flex w-[100%] justify-center items-center mt-[10px]'>
-        <div className='h-[400px] w-[300px]' >
-            <div className='flex flex-col h-[60%] items-center justify-center'>
-               <div className='divimg border-2 border-[#f08800] rounded-full w-[150px] h-[150px] '></div>
-               <p className='font-[600]'>Chukwuma Praise</p>
-               <p>Student @FUNAI</p>
+        <div className='w-[95%]'>
+        <Slider {...settings} className="">
+            <div>
+                <div className='h-[400px] w-[300px]' >
+                    <div className='flex flex-col h-[60%] items-center justify-center'>
+                    <div className='divimg border-2 border-[#f08800] rounded-full w-[150px] h-[150px] '></div>
+                    <p className='font-[600]'>Chukwuma Praise</p>
+                    <p>Student @FUNAI</p>
+                    </div>
+                    <div className='flex items-center justify-center h-[40%]'>
+                    <div className='basis-[10%] ml-[5px] h-[100%] flex flex-col justify-center items-center'>
+                        <div className='bg-[#f08800] rounded-full w-[10px] h-[10px] mb-[2px]'></div>
+                        <div className='border-l-4  border-[#f08800] h-[80%]'></div>
+                        <div className='bg-[#f08800] rounded-full w-[10px] h-[10px] mt-[2px] '></div>
+                    </div>
+                    <p className='basis-[90%] text-center'>Excellent tutors, tranquil community and a well structured program. I just want to commend the CEO for establishing this project and having it shape lives for the long term in the Financial world.</p>
+                    </div>
+                </div>
             </div>
-            <div className='flex items-center justify-center h-[40%] '>
-              <div className='basis-[20%] ml-[5px] h-[100%] flex flex-col justify-center items-center'>
-                <div className='bg-[#f08800] rounded-full w-[10px] h-[10px] mb-[2px]'></div>
-                <div className='border-l-4  border-[#f08800] h-[80%]'></div>
-                <div className='bg-[#f08800] rounded-full w-[10px] h-[10px] mt-[2px] '></div>
-              </div>
-              <p className='basis-[80%] text-center'>Excellent tutors, tranquil community and a well structured program. I just want to commend the CEO for establishing this project and having it shape lives for the long term in the Financial world.</p>
+            <div>
+                <div className='h-[400px] w-[300px]' >
+                    <div className='flex flex-col h-[60%] items-center justify-center'>
+                    <div className='divimg2 border-2 border-[#f08800] rounded-full w-[150px] h-[150px] '></div>
+                    <p className='font-[600]'>Obasi Mmeso</p>
+                    <p>Student @EBSU</p>
+                    </div>
+                    <div className='flex items-center justify-center h-[40%]'>
+                    <div className='basis-[10%] ml-[5px] h-[100%] flex flex-col justify-center items-center'>
+                        <div className='bg-[#f08800] rounded-full w-[10px] h-[10px] mb-[2px]'></div>
+                        <div className='border-l-4  border-[#f08800] h-[80%]'></div>
+                        <div className='bg-[#f08800] rounded-full w-[10px] h-[10px] mt-[2px] '></div>
+                    </div>
+                    <p className='basis-[90%] text-center'>You are all doing a great job and i commend your work Also. I was wondering if you can be doing like a little promo where you'll be giving members of the challenge a chance to be in your masterclass or be giving members account with little funds</p>
+                    </div>
+                </div>
             </div>
-        </div>
+        </Slider>
         </div>
     </div>
   )
--- components/about/About.js
+++ components/about/About.js
@@ -11,10 +11,10 @@ export default function About() {
     <div className='about'>
         <div className='aboutoverlay'>
           <div className='w-[100%] sm:w-[50%] m-[3px] h-[100%] flex flex-col justify-center items-center '>
-            <h1 data-aos="fade-down" className='text-[#f08800] w-[80%]  pl-[5px] font-[700] text-[30px] font-danc '>The best way to master the financial market and make unfair profits from it.</h1>
+            <h1 data-aos="fade-down" className='text-[#f08800] w-[80%]  pl-[5px] font-[700] sm:text-[30px] text-[25px] font-danc '>The best way to master the financial market and make unfair profits from it.</h1>
             <h2 data-aos="zoom-in" className='text-white mt-[3px] mb-[3px] font-pop w-[80%]'>XYZ Academy is a brand that gives you the opportunity to master, practice and earn from the Financial market</h2>
             <h2 data-aos="zoom-in" className='text-white w-[80%]  hidden md:block'>We also teach tech skills that will help you to make more money by solving Problems in business and society at large</h2>
-            <div className='bg-[hsl(34,100%,47%)] w-[40%] sm:w-[40%] md:w-[50%] text-center rounded-xl border border-white mt-[15px] p-[10px] font-[600]'>Register With Us</div>
+            <div className='bg-[hsl(34,100%,47%)] w-[50%] sm:w-[40%] md:w-[50%] text-center rounded-xl border border-white mt-[15px] p-[10px] font-[600]'>Register With Us</div>
           </div>
         </div>
     </div>
