9d1a7024b44ffd216ac5304af8adda10f14ba7bd
geepy technologies <geepytechnologies@gmail.com>
2022-08-27 23:13:59 +0100

    changed a faulty code

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ export default function Home() {
       </div>
       <Goals />
       <Events />
-      // <Services />
+       {/* Services */} 
       <Courses />
       <Comments />
       <Footer />
